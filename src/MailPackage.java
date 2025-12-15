15181a796a83207622c518a2be5019efda3e81ab
LOGPVR <214538967+LOGPVR@users.noreply.github.com>
2025-12-15 22:19:05 +0300

    created the class structure from the assigment

---
+++ src/MailPackage.java
@@ -0,0 +1,26 @@
+/*
+Второй класс описывает почтовую посылку:
+Посылка, содержимое которой можно получить с помощью метода `getContent`
+*/
+
+public class MailPackage extends AbstractSendable{
+    private final Package content;
+
+    protected MailPackage(String from, String to, Package content) {
+        super(from, to);
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        MailPackage that = (MailPackage) o;
+
+        if (!content.equals(that.content)) return false;
+
+        return true;
+    }
+}
